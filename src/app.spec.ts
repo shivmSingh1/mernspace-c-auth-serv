625849d5f58f464050e19b81d0abcd4ab43b4efd
shivmSingh1 <shivams7380@gmail.com>
2026-07-14 15:00:22 +0000

    add supertest

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,9 +1,16 @@
 // import { describe, it } from "node:test";
+import app from './app';
 import { calculateDiscount } from './utils';
+import request from 'supertest';
 
 describe('App', () => {
     it('should return correct discount amount', () => {
         const discount = calculateDiscount(100, 10);
         expect(discount).toBe(10);
+    });
+
+    it('should be return 200 status code', async () => {
+        const response = await request(app).get('/').send();
+        expect(response.statusCode).toBe(200);
     });
 });

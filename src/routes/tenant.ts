3ccb2867be5a29ec7de45dea7f27af521b68dd6f
shivmSingh1 <shivams7380@gmail.com>
2026-07-31 12:32:26 +0000

    feat: can access middleware

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -4,6 +4,8 @@ import { TenantService } from '../services/TenantService';
 import { AppDataSource } from '../config/data-source';
 import { Tenant } from '../entities/Tenants';
 import authenticate from '../middlewares/authenticate';
+import { canAccess } from '../middlewares/canAccess';
+import { Roles } from '../constants';
 const tenantRouter = express.Router();
 
 const tenantRepository = AppDataSource.getRepository(Tenant);
@@ -11,8 +13,11 @@ const tenantRepository = AppDataSource.getRepository(Tenant);
 const tenantService = new TenantService(tenantRepository);
 const tenantController = new TenantController(tenantService);
 
-tenantRouter.post('/', authenticate, (req, res, next) =>
-    tenantController.create(req, res, next),
+tenantRouter.post(
+    '/',
+    authenticate,
+    canAccess([Roles.ADMIN]),
+    (req, res, next) => tenantController.create(req, res, next),
 );
 
 export default tenantRouter;
